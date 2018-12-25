8c18b955f3eaa8e2fc41b66d34e4a2b77250b23f
ohta6 <ohta@cmu.iit.tsukuba.ac.jp>
2018-12-25 08:18:42 +0000

    fashion mnist

--- capsulenet.py
+++ capsulenet.py
@@ -190,8 +190,9 @@ def manipulate_latent(model, data, args):
 
 def load_mnist():
     # the data, shuffled and split between train and test sets
-    from keras.datasets import mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    import keras
+    fashion_mnist = keras.datasets.fashion_mnist
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
     x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
     x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
